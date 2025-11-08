875c9ad5539e54140d45ebdd3ceb736813ea35c1
Nasim <nasim.reja.077@gmail.com>
2025-11-09 00:43:35 +0530

    Add Advance Search Functionality

--- RestAPI/Product-Hunt/src/controllers/product.controller.js
+++ RestAPI/Product-Hunt/src/controllers/product.controller.js
@@ -2,7 +2,25 @@ import productModel from "../models/product.model.js";
 
 export const getAllProducts = async(req, res)=>{
      try {
-          const myProductsData = await productModel.find({});
+          const { company, name, featured } = req.query;
+          const queryObject = {};
+
+          if (company){
+               queryObject.company = company;
+               // console.log(queryObject);
+          }
+
+          if (name) {
+               queryObject.name = { $regex: name, $option: "i"};
+          }
+
+          if (featured) {
+               queryObject.featured = featured;
+          }
+
+          console.log(queryObject);
+
+          const myProductsData = await productModel.find(req.query);
           res.status(200).json({ success: true, myProductsData });
      } catch (error) {
           res.status(500).json({ success: false, error: error.message })
