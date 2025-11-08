476dface76e8ab260d63398a602d0f3bc9713c6e
Nasim <nasim.reja.077@gmail.com>
2025-11-09 01:24:42 +0530

    Add SORT functionality and Return Specific Document Fields using SELECT

--- RestAPI/Product-Hunt/src/controllers/product.controller.js
+++ RestAPI/Product-Hunt/src/controllers/product.controller.js
@@ -2,7 +2,7 @@ import productModel from "../models/product.model.js";
 
 export const getAllProducts = async(req, res)=>{
      try {
-          const { company, name, featured } = req.query;
+          const { company, name, featured, sort, select } = req.query;
           const queryObject = {};
 
           if (company){
@@ -10,17 +10,36 @@ export const getAllProducts = async(req, res)=>{
                // console.log(queryObject);
           }
 
-          if (name) {
-               queryObject.name = { $regex: name, $option: "i"};
-          }
+           if (name) {
+      queryObject.name = { $regex: name, $options: "i" }; // ✅ fixed
+    }
+
+           // First create query
+          let apiData = productModel.find(queryObject);
 
           if (featured) {
                queryObject.featured = featured;
           }
 
+           // Sorting (sort=name,price)
+
+          if (sort) {
+               let sortFix = sort.replace(",", " ");
+               // queryObject.sort = sortFix;
+               apiData = apiData.sort(sortFix);
+          }
+
+           // Selecting Fields (select=name,company)
+          if(select){
+               // let selectFix = select.replace("," , " ");
+               // apiData = apiData.select(selectFix)
+               let selectFix = select.split(",").join(" ");
+               apiData = apiData.select(selectFix)
+          }
+
           console.log(queryObject);
 
-          const myProductsData = await productModel.find(req.query);
+          const myProductsData = await apiData;
           res.status(200).json({ success: true, myProductsData });
      } catch (error) {
           res.status(500).json({ success: false, error: error.message })
