8159ebb260c22fb984a4736f617a646bd8d414dd
Nasim <nasim.reja.077@gmail.com>
2025-11-09 01:47:39 +0530

    Add Pagination

--- RestAPI/Product-Hunt/src/controllers/product.controller.js
+++ RestAPI/Product-Hunt/src/controllers/product.controller.js
@@ -37,10 +37,23 @@ export const getAllProducts = async(req, res)=>{
                apiData = apiData.select(selectFix)
           }
 
+           // Pagination
+           
+          let page = Number(req.query.page) || 1;
+          let limit= Number(req.query.limit) || 3;
+          let skip = (page - 1) * limit;
+          // page = 2
+          // limit = 3;
+          // skip = 1 * 3 = 3
+          apiData = apiData.skip(skip).limit(limit);
           console.log(queryObject);
 
           const myProductsData = await apiData;
-          res.status(200).json({ success: true, myProductsData });
+          res.status(200).json({ 
+               success: true, 
+               total: myProductsData.length, page, limit, 
+               data: myProductsData, 
+          });
      } catch (error) {
           res.status(500).json({ success: false, error: error.message })
      }
