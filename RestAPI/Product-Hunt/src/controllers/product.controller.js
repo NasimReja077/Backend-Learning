264ee633f2f37803fee714c0fcfb18c8cb82b7c4
Nasim <nasim.reja.077@gmail.com>
2025-11-09 09:37:34 +0530

    Add category, rating

--- RestAPI/Product-Hunt/src/controllers/product.controller.js
+++ RestAPI/Product-Hunt/src/controllers/product.controller.js
@@ -2,34 +2,44 @@ import productModel from "../models/product.model.js";
 
 export const getAllProducts = async(req, res)=>{
      try {
-          const { company, name, featured, sort, select } = req.query;
+          const { company, name, featured, sort, select, category, rating } = req.query;
           const queryObject = {};
 
+          // Filter by company
           if (company){
                queryObject.company = company;
                // console.log(queryObject);
           }
+          
+          if(name){
+               queryObject.name = { $regex: name, $options: "i" };
+          }
 
-           if (name) {
-      queryObject.name = { $regex: name, $options: "i" }; // ✅ fixed
-    }
-
-           // First create query
+          // First create DB query
           let apiData = productModel.find(queryObject);
 
+          // Filter by featured
           if (featured) {
                queryObject.featured = featured;
           }
+          // Filter by Category
+          if (category){
+               queryObject.category = category;
+          }
 
-           // Sorting (sort=name,price)
+          // Filter by Rating >= given value
+          if (rating){
+               queryObject.rating = { $gte: Number(rating)};
+          }
 
+          // Sorting (sort=name,price)
           if (sort) {
                let sortFix = sort.replace(",", " ");
                // queryObject.sort = sortFix;
                apiData = apiData.sort(sortFix);
           }
 
-           // Selecting Fields (select=name,company)
+          // Selecting Fields (select=name,company)
           if(select){
                // let selectFix = select.replace("," , " ");
                // apiData = apiData.select(selectFix)
@@ -37,8 +47,7 @@ export const getAllProducts = async(req, res)=>{
                apiData = apiData.select(selectFix)
           }
 
-           // Pagination
-           
+          // Pagination 
           let page = Number(req.query.page) || 1;
           let limit= Number(req.query.limit) || 3;
           let skip = (page - 1) * limit;
@@ -49,6 +58,7 @@ export const getAllProducts = async(req, res)=>{
           console.log(queryObject);
 
           const myProductsData = await apiData;
+
           res.status(200).json({ 
                success: true, 
                total: myProductsData.length, page, limit, 
