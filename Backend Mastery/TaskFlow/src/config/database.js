cc5831279832a2ebdea5b21dae32df4431772e61
Nasim <nasim.reja.077@gmail.com>
2026-07-06 23:12:26 +0530

    feat: Refactor MongoDB connection to use ES6 modules and improve timeout settings

--- Backend Mastery/TaskFlow/src/config/database.js
+++ Backend Mastery/TaskFlow/src/config/database.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB Connected Successfully');
-  } catch (error) {
-    console.error('❌ MongoDB Connection Failed:', error.message);
+  try{
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMs: 5000, // Timeout after 5s instead of 30s
+    });
+    console.log(`✅ MongoDB Connected Successfully ${conn.connection.host}`);
+  }catch (err){
+    console.error("❌ MongoDB Connection Failed:", err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
